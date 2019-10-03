9d15b577072f3399e6d1c2cc3016cacfd525410f
Kyle West <kyle@familysearch.org>
2019-10-03 13:53:47 -0600

    walk through each tag and read packages

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
-const git = require('simple-git')(process.cwd());
-const path = require('path')
-git.tag([], (err, stdout) => {
-  let tags = [...new Set(stdout.split('\n').filter(x => !!x).map(x => x.startsWith('v') ? x.substring(1) : x))]
-  tags.forEach(tag => git.checkout(tag, (err, data) => {
-    let pack = require(path.resolve(process.cwd(), './package.json'))
-    console.log(data);
-    console.log(pack);
-  }))
-})
+const { makePackagesFromTags } = require('./lib/packages-from-tags')
+
+
+const [ action, ...args ] = process.argv.slice(2)
+
+switch (action) {
+  // ghpm sync-packages-from-tags
+  case 'sync-packages-from-tags': {
+    makePackagesFromTags((err) => {
+      if (err) throw err
+      console.log('Done')
+    })
+    break;
+  }
+  default : {
+    console.log('I do not know what you want by', action)
+  }
+}
